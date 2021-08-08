859c3b290b21e03874361bc67601be5c1869cb98
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-08-08 00:15:30 -0400

    Add additional test cases for Checks

--- tests/test_banana.py
+++ tests/test_banana.py
@@ -1,4 +1,3 @@
-from Permission import Permission
 from Perm import Perm
 from Check import Check
 from banana import banana
@@ -23,3 +22,30 @@ def test_with_check():
     test = Test(0)
     assert test.test2 == 0
     assert test.test
+
+
+def test_inside_class():
+    class Test:
+        test = Check(lambda *_: True)
+
+        def __init__(self, test):
+            self.test2 = test
+
+    test = Test(0)
+    assert test.test2 == 0
+    assert test.test
+
+
+def test_inside_instance():
+    class Test:
+        test = Check(lambda self: getattr(self, "test2"))
+
+        def __init__(self, test):
+            self.test2 = test
+
+    test = Test(True)
+    test2 = Test(False)
+    assert test.test2
+    assert test.test
+    assert not test2.test2
+    assert not test2.test
